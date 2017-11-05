afcde4203759cdde3ad188c81e33a1557410acc6
moonerTT <1415903806@qq.com>
2017-11-05 17:46:27 +0800

    init

---
+++ src/main/java/com/clan/Run.java
@@ -0,0 +1,25 @@
+package com.clan;
+
+import io.undertow.Undertow;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.Headers;
+
+/**
+ * Created by robot on 2017/11/5.
+ */
+public class Run {
+
+    public static void main(final String[] args) {
+        Undertow server = Undertow.builder()
+                .addHttpListener(8080, "localhost")
+                .setHandler(new HttpHandler() {
+
+                    public void handleRequest(final HttpServerExchange exchange) throws Exception {
+                        exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
+                        exchange.getResponseSender().send("Hello World");
+                    }
+                }).build();
+        server.start();
+    }
+}
